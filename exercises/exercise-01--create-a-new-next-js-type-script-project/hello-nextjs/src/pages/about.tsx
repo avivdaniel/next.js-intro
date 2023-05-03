ef5bb8ebee77c9c71eee49ec1bd19d8700255a3e
adaniel <adaniel@digibank.co.il>
2023-05-03 11:53:44 +0300

    :tada: Exercise 2 - create a new route

---
+++ exercises/exercise-01--create-a-new-next-js-type-script-project/hello-nextjs/src/pages/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const About = () => {
+    return (
+        <>
+            <h1>
+                About me
+            </h1>
+            <Link href="/">
+                Back to home page
+            </Link>
+        </>
+    );
+};
+
+export default About;
