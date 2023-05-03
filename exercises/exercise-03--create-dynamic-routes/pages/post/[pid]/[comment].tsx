00786d50dd87b5b63bfb727636374be7564997dc
adaniel <adaniel@digibank.co.il>
2023-05-03 12:05:53 +0300

    :tada: Exercise 3 - dynamic nested routes

---
+++ exercises/exercise-03--create-dynamic-routes/pages/post/[pid]/[comment].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useRouter} from "next/router";
+
+const Comment = () => {
+    const router = useRouter();
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Comment;
