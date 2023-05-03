191ddac2235db9a0c9b2b58398c21e6a41524256
adaniel <adaniel@digibank.co.il>
2023-05-03 12:29:00 +0300

    :tada: Exercise 5 - Image optimizations and remote images

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,14 @@
 // ✍️ import the Image component from 'next/image'
-
+import Image from "next/image";
 // ✍️ import "lazar.png" statically
+import lazar from '../lazar.png';
 
+const DUA_IMAGE_URL = 'https://assets.vogue.com/photos/642c579f38d6f760cd23e8a1/4:3/w_1635,h_1226,c_limit/BARBIE_Character_DUA_InstaVert_1638x2048_DOM.jpg'
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+    return <>
+        <Image src={lazar} alt="Lazar picture"/>
+        <Image src={DUA_IMAGE_URL} alt="dua lipa image" width={250} height={250}/>
+    </>
 }
 
 export default Home
