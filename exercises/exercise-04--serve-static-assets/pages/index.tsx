06580f0e5e5dc377786be7fcaab28448580b710f
adaniel <adaniel@digibank.co.il>
2023-05-03 12:21:14 +0300

    :tada: Exercise 4 - static assets

--- exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -1,11 +1,12 @@
 const Home = () => {
-  return (
-    <div>
-      <h2>Hello 👋</h2>
-      <p>My name is Lazar Nikolov</p>
-      {/* ✍️ add the "lazar.png" image here */}
-    </div>
-  )
+    return (
+        <div>
+            <h2>Hello 👋</h2>
+            <p>My name is Lazar Nikolov</p>
+            <img src="/lazar.png"/>
+            {/* ✍️ add the "lazar.png" image here */}
+        </div>
+    )
 }
 
 export default Home
