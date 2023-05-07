648206f02563cef902b5c64e4605897103af6194
adaniel <adaniel@digibank.co.il>
2023-05-07 11:45:46 +0300

    :tada: Exercise 12 - SSG (static generation - generates data at build time)

---
+++ exercises/exercise-12--use-the-static-generation-method/pages/[genre].tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {GetStaticPaths, GetStaticProps} from "next";
+
+type Props = {
+    genre: string
+}
+const Genre = ({genre}: Props) => {
+    return (
+        <div>
+            Hello {genre}
+        </div>
+    );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+    return {
+        paths: ['rock', 'country', 'pop'].map(genre => ({params: {genre}})),
+        fallback: false // False - 404 page if not exists
+    }
+}
+
+export const getStaticProps: GetStaticProps<Props> = (context) => {
+    const genre = context.params?.genre as string;
+
+    if (!genre) {
+        return {
+            notFound: true
+        }
+    };
+
+    return {
+        props: {
+            genre
+        }
+    }
+};
+
+
+export default Genre;
--- exercises/exercise-12--use-the-static-generation-method/pages/index.tsx
+++ exercises/exercise-12--use-the-static-generation-method/pages/index.tsx
@@ -1,23 +1,35 @@
 // ✍️ import GetStaticProps from 'next'
-import { List, ListItem, Container, Heading } from '@chakra-ui/react'
+import {List, ListItem, Container, Heading} from '@chakra-ui/react'
+import {GetStaticProps} from "next";
+import Link from "next/link";
 
 type Props = {
-  genres: string[]
+    genres: string[]
 }
 
-const Home = ({ genres }: Props) => {
-  return (
-    <Container maxW="container.xl" pt={10}>
-      <Heading mb={4}>Genres:</Heading>
-      <List>
-        {genres.map((genre) => (
-          <ListItem key={genre}>{genre}</ListItem>
-        ))}
-      </List>
-    </Container>
-  )
+const Home = ({genres}: Props) => {
+    return (
+        <Container>
+            <Heading mb={4}>Genres:</Heading>
+            <List>
+                {genres.map((genre) => (
+                    <ListItem key={genre}>
+                        <Link href={`/${genre}`}>
+                            {genre}
+                        </Link>
+                    </ListItem>
+                ))}
+            </List>
+        </Container>
+    )
 }
 
-// ✍️ export the getStaticProps method and return an array of genres as props
-
+// ✍️ export the getStaticProps method and return an array of genres as props - safe for sb connection
+export const getStaticProps: GetStaticProps<Props> = () => {
+    return {
+        props: {
+            genres: ['rock', 'country', 'pop', 'indie']
+        }
+    }
+}
 export default Home
